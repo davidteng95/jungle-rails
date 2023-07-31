987ee4ab767c7c31d507756058e1aa7ef14a6183
David <davidteng95@gmail.com>
2023-07-31 04:41:14 +0000

    implement signup route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   get 'about', to: 'about#index'
 
+  get'/signup', to: 'users#new'
+  get'/users', to: 'user#create'
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show, :index]
 
